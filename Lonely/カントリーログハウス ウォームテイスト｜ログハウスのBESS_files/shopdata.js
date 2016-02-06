c494d389977d3da6f8ada4e32fb1495cd22fdb02
sasagawa yo <sasagawa.yo@gmail.com>
2016-02-06 18:28:21 +0900

    corpsite first commit

---
+++ Lonely/カントリーログハウス ウォームテイスト｜ログハウスのBESS_files/shopdata.js
@@ -0,0 +1,433 @@
+var shopData = [
+  {
+      "name" : "BESS札幌",
+      "address" : "〒069-0806 北海道江別市新栄台2番地の23",
+      "siteurl" : "http://sapporo.bess.jp/",
+      "mapurl" : "http://sapporo.bess.jp/map/",
+      "tel" : "011-391-6664",
+      "products" : ["cw","w","j","d","h"],
+      "lat" : "43.1117429",
+      "lon" : "141.5099848"
+  },
+  {
+      "name" : "BESS青森",
+      "address" : "〒039-1212 青森県三戸郡階上町蒼前西1-9-3171",
+      "siteurl" : "http://aomori.bess.jp/",
+      "mapurl" : "http://aomori.bess.jp/map/",
+      "tel" : "0178-88-1600",
+      "products" : ["cw","a","w","j","d"],
+      "lat" : "40.4791096",
+      "lon" : "141.5702741"
+  },
+  {
+      "name" : "BESS秋田",
+      "address" : "〒018-1401 秋田県潟上市昭和大久保北野細谷道添72-165",
+      "siteurl" : "http://akita.bess.jp/",
+      "mapurl" : "http://akita.bess.jp/map/",
+      "tel" : "018-855-5333",
+      "products" : ["cc","a","w","d"],
+      "lat" : "39.8373582",
+      "lon" : "140.0512373"
+  },
+  {
+      "name" : "BESS盛岡",
+      "address" : "〒020-0051 岩手県盛岡市下太田下川原50-8",
+      "siteurl" : "http://morioka.bess.jp/",
+      "mapurl" : "http://morioka.bess.jp/map/",
+      "tel" : "019-656-2277",
+      "products" : ["cw","a","w","j"],
+      "lat" : "39.6968738",
+      "lon" : "141.1203094"
+  },
+  {
+      "name" : "BESS仙台",
+      "address" : "〒981-3363 宮城県黒川郡富谷町杜乃橋2-1-7",
+      "siteurl" : "http://sendai.bess.jp/",
+      "mapurl" : "http://sendai.bess.jp/map/",
+      "tel" : "022-779-0760",
+      "products" : ["cc","f","a","w","j","d"],
+      "lat" : "38.385248",
+      "lon" : "140.873255"
+  },
+  {
+      "name" : "BESS山形",
+      "address" : "〒990-2473 山形県山形市松栄2-1-45",
+      "siteurl" : "http://yamagata.bess.jp/",
+      "mapurl" : "http://yamagata.bess.jp/map/",
+      "tel" : "023-646-2220",
+      "products" : ["cw","f","w","j"],
+      "lat" : "38.2399947",
+      "lon" : "140.2934821"
+  },
+  {
+      "name" : "BESS福島",
+      "address" : "〒963-8841 福島県郡山市山崎305-65",
+      "siteurl" : "http://fukushima.bess.jp/",
+      "mapurl" : "http://fukushima.bess.jp/map/",
+      "tel" : "024-925-9595",
+      "products" : ["cw","f","w","j","d","h"],
+      "lat" : "37.3822947",
+      "lon" : "140.3593698"
+  },
+  {
+      "name" : "BESS長野",
+      "address" : "〒381-0031 長野県長野市西尾張部1119-1",
+      "siteurl" : "http://nagano.bess.jp/",
+      "mapurl" : "http://nagano.bess.jp/map/",
+      "tel" : "026-239-7201",
+      "products" : ["cw","f","w","j","h"],
+      "lat" : "36.6477041",
+      "lon" : "138.2297722"
+  },
+  {
+      "name" : "BESS駒ヶ根",
+      "address" : "〒399-4117 長野県駒ヶ根市赤穂23-195",
+      "siteurl" : "http://komagane.bess.jp/",
+      "mapurl" : "http://komagane.bess.jp/map/",
+      "tel" : "0265-81-1400",
+      "products" : ["cw","f","a","w","j","d","h"],
+      "lat" : "35.7310078",
+      "lon" : "137.9011593"
+  },
+  {
+      "name" : "BESS新潟",
+      "address" : "〒950-0932 新潟県新潟市中央区長潟386番地",
+      "siteurl" : "http://niigata.bess.jp/",
+      "mapurl" : "http://niigata.bess.jp/map/",
+      "tel" : "025-282-5691",
+      "products" : ["cw","w"],
+      "lat" : "37.883597",
+      "lon" : "139.067669"
+  },
+  {
+      "name" : "BESS長岡",
+      "address" : "〒940-0872 新潟県長岡市稲葉町219-17",
+      "siteurl" : "http://nagaoka.bess.jp/",
+      "mapurl" : "http://nagaoka.bess.jp/map/",
+      "tel" : "0258-24-6610",
+      "products" : ["cc","w","d"],
+      "lat" : "37.4781645",
+      "lon" : "138.8769785"
+  },
+  {
+      "name" : "BESS富山",
+      "address" : "〒930-0834 富山県富山市問屋町1-28-5",
+      "siteurl" : "http://toyama.bess.jp/",
+      "mapurl" : "http://toyama.bess.jp/map/",
+      "tel" : "076-471-6100",
+      "products" : ["cc","a","w","d"],
+      "lat" : "36.7012094",
+      "lon" : "137.2492619"
+  },
+  {
+      "name" : "BESS金沢",
+      "address" : "〒920-0370 石川県金沢市上安原1-201",
+      "siteurl" : "http://kanazawa.bess.jp/",
+      "mapurl" : "http://kanazawa.bess.jp/map/",
+      "tel" : "076-240-6140",
+      "products" : ["cw","f","h"],
+      "lat" : "36.564591",
+      "lon" : "136.590917"
+  },
+  {
+      "name" : "BESS栃木",
+      "address" : "〒320-0066 栃木県宇都宮市駒生2-907-1",
+      "siteurl" : "http://tochigi.bess.jp/",
+      "mapurl" : "http://tochigi.bess.jp/map/",
+      "tel" : "028-643-1177",
+      "products" : ["cw","f","w","j","d","h"],
+      "lat" : "36.5753141",
+      "lon" : "139.8497278"
+  },
+  {
+      "name" : "BESS水戸",
+      "address" : "〒311-4143 茨城県水戸市大塚町1465-1",
+      "siteurl" : "http://mito.bess.jp/",
+      "mapurl" : "http://mito.bess.jp/map/",
+      "tel" : "029-253-5466",
+      "products" : ["cw","f","w","j","h"],
+      "lat" : "36.3858651",
+      "lon" : "140.3904111"
+  },
+  {
+      "name" : "BESSつくば",
+      "address" : "〒305-0868 茨城県つくば市台町3丁目7-6",
+      "siteurl" : "http://tsukuba.bess.jp/",
+      "mapurl" : "http://tsukuba.bess.jp/map/",
+      "tel" : "029-836-7798",
+      "products" : ["cw","a","w","d"],
+      "lat" : "36.0355778",
+      "lon" : "140.0856233"
+  },
+  {
+      "name" : "BESS東葛",
+      "address" : "〒277-0015 千葉県柏市弥生町8番55号",
+      "siteurl" : "http://tokatsu.bess.jp/",
+      "mapurl" : "http://tokatsu.bess.jp/map/",
+      "tel" : "04-7197-1531",
+      "products" : ["cc","w","d"],
+      "lat" : "35.855445",
+      "lon" : "139.987590"
+  },
+  {
+      "name" : "BESS高崎",
+      "address" : "〒370-0002 群馬県高崎市日高町1052-1",
+      "siteurl" : "http://takasaki.bess.jp/",
+      "mapurl" : "http://takasaki.bess.jp/map/",
+      "tel" : "027-364-5123",
+      "products" : ["cc","a","w","j","d"],
+      "lat" : "36.353128",
+      "lon" : "139.037400"
+  },
+  {
+      "name" : "BESS熊谷",
+      "address" : "〒360-0032 埼玉県熊谷市銀座5-2-23",
+      "siteurl" : "http://kumagaya.bess.jp/",
+      "mapurl" : "http://kumagaya.bess.jp/map/",
+      "tel" : "048-594-9015",
+      "products" : ["cc","w","j","d"],
+      "lat" : "36.1424093",
+      "lon" : "139.3976816"
+  },
+  {
+      "name" : "東京・代官山 総合展示場「BESSスクエア」",
+      "address" : "〒153-0042 東京都目黒区青葉台1-4-5",
+      "siteurl" : "http://square.bess.jp/",
+      "mapurl" : "http://square.bess.jp/map/",
+      "tel" : "03-3462-7000(代)",
+      "products" : ["cc","g","a","w","j","d"],
+      "lat" : "35.6494897",
+      "lon" : "139.6977599"
+  },
+  {
+      "name" : "BESS藤沢",
+      "address" : "〒251-0861 神奈川県藤沢市大庭5224-1",
+      "siteurl" : "http://fujisawa.bess.jp/",
+      "mapurl" : "http://fujisawa.bess.jp/map/",
+      "tel" : "0466-88-0080",
+      "products" : ["cc","a","w","j","d"],
+      "lat" : "35.363022",
+      "lon" : "139.4541586"
+  },
+  {
+      "name" : "BESS岐阜",
+      "address" : "〒500-8212 岐阜県岐阜市日野南1-4-1",
+      "siteurl" : "http://gifu.bess.jp/",
+      "mapurl" : "http://gifu.bess.jp/map/",
+      "tel" : "058-214-9520",
+      "products" : ["cc","f","a","w","j"],
+      "lat" : "35.4290368",
+      "lon" : "136.7976721"
+  },
+  {
+      "name" : "BESS富士",
+      "address" : "〒417-0061 静岡県富士市伝法676-3",
+      "siteurl" : "http://fuji.bess.jp/",
+      "mapurl" : "http://fuji.bess.jp/map/",
+      "tel" : "0545-73-1077",
+      "products" : ["cw","f","w","j"],
+      "lat" : "35.1854928",
+      "lon" : "138.669662"
+  },
+  {
+      "name" : "BESS静岡中部",
+      "address" : "〒421-0304 静岡県榛原郡吉田町神戸1553-2",
+      "siteurl" : "http://shizuokachubu.bess.jp/",
+      "mapurl" : "http://shizuokachubu.bess.jp/map/",
+      "tel" : "050-3816-3711",
+      "products" : ["cc","w"],
+      "lat" : "34.790403",
+      "lon" : "138.240335"
+  },
+  {
+      "name" : "BESS浜松",
+      "address" : "〒432-8069 静岡県浜松市西区志都呂一丁目22番19号",
+      "siteurl" : "http://hamamatsu.bess.jp/",
+      "mapurl" : "http://hamamatsu.bess.jp/map/",
+      "tel" : "053-440-5333",
+      "products" : ["cw","f","w","j","d"],
+      "lat" : "34.6995133",
+      "lon" : "137.6604221"
+  },
+  {
+      "name" : "BESS蒲郡",
+      "address" : "〒443-0014 愛知県蒲郡市海陽町2丁目7-3",
+      "siteurl" : "http://gamagori.bess.jp/",
+      "mapurl" : "http://gamagori.bess.jp/map/",
+      "tel" : "0533-59-7400",
+      "products" : ["f","a","w","j"],
+      "lat" : "34.8099279",
+      "lon" : "137.2810372"
+  },
+  {
+      "name" : "BESS東愛知",
+      "address" : "〒471-0058 愛知県豊田市大池町汐取64-3",
+      "siteurl" : "http://higashiaichi.bess.jp/",
+      "mapurl" : "http://higashiaichi.bess.jp/map/",
+      "tel" : "0565-36-6036",
+      "products" : ["cc","a","w","j","d"],
+      "lat" : "35.1091335",
+      "lon" : "137.118483"
+  },
+  {
+      "name" : "BESS北愛知",
+      "address" : "〒485-0803 愛知県小牧市高根2-194",
+      "siteurl" : "http://kitaaichi.bess.jp/",
+      "mapurl" : "http://kitaaichi.bess.jp/map/",
+      "tel" : "0568-47-0121",
+      "products" : ["cw","w","j"],
+      "lat" : "35.2935059",
+      "lon" : "136.9772646"
+  },
+  {
+      "name" : "BESS南愛知",
+      "address" : "〒477-0031 愛知県東海市大田町与五八34",
+      "siteurl" : "http://minamiaichi.bess.jp/",
+      "mapurl" : "http://minamiaichi.bess.jp/map/",
+      "tel" : "0562-33-7770",
+      "products" : ["cw","a","w","j"],
+      "lat" : "35.010385",
+      "lon" : "136.899245"
+  },
+  {
+      "name" : "BESS三重",
+      "address" : "〒513-0809 三重県鈴鹿市西条5丁目81",
+      "siteurl" : "http://mie.bess.jp/",
+      "mapurl" : "http://mie.bess.jp/map/",
+      "tel" : "059-381-7337",
+      "products" : ["cw","f","w","j"],
+      "lat" : "34.8797656",
+      "lon" : "136.5699294"
+  },
+  {
+      "name" : "BESS京滋",
+      "address" : "〒524-0101 滋賀県守山市今浜町2532-7",
+      "siteurl" : "http://keiji.bess.jp/",
+      "mapurl" : "http://keiji.bess.jp/map/",
+      "tel" : "077-585-0694",
+      "products" : ["cc","f","w","j"],
+      "lat" : "35.1187373",
+      "lon" : "135.9464265"
+  },
+  {
+      "name" : "BESS神戸",
+      "address" : "〒651-1352 兵庫県神戸市北区八多町下小名田550-1",
+      "siteurl" : "http://kobe.bess.jp/",
+      "mapurl" : "http://kobe.bess.jp/map/",
+      "tel" : "078-951-6666",
+      "products" : ["cw","f","a","w"],
+      "lat" : "34.8418049",
+      "lon" : "135.2129187"
+  },
+  {
+      "name" : "BESS和歌山",
+      "address" : "〒640-0343 和歌山県和歌山市江南225-12",
+      "siteurl" : "http://wakayama.bess.jp/",
+      "mapurl" : "http://wakayama.bess.jp/map/",
+      "tel" : "073-479-2788",
+      "products" : ["j"],
+      "lat" : "34.1926945",
+      "lon" : "135.2241322"
+  },
+  {
+      "name" : "BESS福知山",
+      "address" : "〒620-0804 京都府福知山市石原2丁目283番",
+      "siteurl" : "http://fukuchiyama.bess.jp/",
+      "mapurl" : "http://fukuchiyama.bess.jp/map/",
+      "tel" : "0773-45-8003",
+      "products" : ["g","j"],
+      "lat" : "35.298248",
+      "lon" : "135.179270"
+  },
+  {
+      "name" : "BESS岡山",
+      "address" : "〒700-0952 岡山県岡山市北区平田174-101",
+      "siteurl" : "http://okayama.bess.jp/",
+      "mapurl" : "http://okayama.bess.jp/map/",
+      "tel" : "086-246-8011",
+      "products" : ["cw","f","a","w","d","h"],
+      "lat" : "34.6340636",
+      "lon" : "133.8839954"
+  },
+  {
+      "name" : "BESS広島",
+      "address" : "〒731-3168 広島県広島市安佐南区伴南1-1-11",
+      "siteurl" : "http://hiroshima.bess.jp/",
+      "mapurl" : "http://hiroshima.bess.jp/map/",
+      "tel" : "082-848-1571",
+      "products" : ["cw","f","w","j"],
+      "lat" : "34.4468731",
+      "lon" : "132.3872869"
+  },
+  {
+      "name" : "BESS高松",
+      "address" : "〒761-0301 香川県高松市林町392-1",
+      "siteurl" : "http://takamatsu.bess.jp/",
+      "mapurl" : "http://takamatsu.bess.jp/map/",
+      "tel" : "087-813-8628",
+      "products" : ["cc","w","d"],
+      "lat" : "34.292872",
+      "lon" : "134.069242"
+  },
+  {
+      "name" : "BESS北九州",
+      "address" : "〒811-4331 福岡県遠賀郡遠賀町別府3713-3",
+      "siteurl" : "http://kitakyushu.bess.jp/",
+      "mapurl" : "http://kitakyushu.bess.jp/map/",
+      "tel" : "093-291-1700",
+      "products" : ["cw","f","a","w","j","d","h"],
+      "lat" : "33.8508117",
+      "lon" : "130.6592943"
+  },
+  {
+      "name" : "BESS福岡西",
+      "address" : "〒819-0374 福岡県福岡市西区千里301-1",
+      "siteurl" : "http://fukuokanishi.bess.jp/",
+      "mapurl" : "http://fukuokanishi.bess.jp/map/",
+      "tel" : "092-805-3751",
+      "products" : ["cc","a","w","j","d"],
+      "lat" : "33.5646154",
+      "lon" : "130.2428437"
+  },
+  {
+      "name" : "BESS福岡南",
+      "address" : "〒818-0135 福岡県太宰府市向佐野2-5-9",
+      "siteurl" : "http://fukuokaminami.bess.jp/",
+      "mapurl" : "http://fukuokaminami.bess.jp/map/",
+      "tel" : "092-928-4169",
+      "products" : ["cw","f","a","w"],
+      "lat" : "33.50881",
+      "lon" : "130.4966449"
+  },
+  {
+      "name" : "BESS宮崎",
+      "address" : "〒880-2112 宮崎県宮崎市大字小松1444-1",
+      "siteurl" : "http://miyazaki.bess.jp/",
+      "mapurl" : "http://miyazaki.bess.jp/map/",
+      "tel" : "0985-48-0022",
+      "products" : ["cw","w","d"],
+      "lat" : "31.931273",
+      "lon" : "131.401285"
+  },
+  {
+      "name" : "BESS鹿児島",
+      "address" : "〒899-2701 鹿児島県鹿児島市石谷町675-1",
+      "siteurl" : "http://kagoshima.bess.jp/",
+      "mapurl" : "http://kagoshima.bess.jp/map/",
+      "tel" : "099-295-0090",
+      "products" : ["cc","w","j"],
+      "lat" : "31.625402",
+      "lon" : "130.467106"
+  },
+  {
+      "name" : "BESS熊本",
+      //"notice" : "<span style='color:#f33'>※一時休業中。8/1（土）再オープン</span>",
+      "address" : "〒862-0951　熊本県熊本市中央区上水前寺2-22-8",
+      "siteurl" : "http://kumamoto.bess.jp/",
+      "mapurl" : "http://kumamoto.bess.jp/map/",
+      "tel" : "096-383-6171",
+      "products" : ["cc"],
+      "lat" : "32.8475632",
+      "lon" : "130.7149737"
+  }
+];
